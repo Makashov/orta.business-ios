e5c2c1caf0a5628c137c06f1ebc17a103d38a78f
Nurbol Makashov <n.makashov@northitgroup.eu>
2026-09-15 20:49:40 +0500

    Initial commit

--- orta.business/OrtaBusinessApp.swift
+++ orta.business/OrtaBusinessApp.swift
@@ -1,9 +1,9 @@
 import SwiftUI
 
-@main struct MyApp: App {
+@main struct OrtaBusinessApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            BottomNavViewView()
         }
     }
 }
